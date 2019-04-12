893560bbca87776328a7d97f34acf9d3a0d73b9a
maxadmaxad <47584987+maxadmaxad@users.noreply.github.com>
2019-04-12 17:20:46 +0600

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-# 3.	По введенным пользователем координатам двух точек вывести
-# уравнение прямой вида y = kx + b, проходящей через эти точки.
-x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
-
-k, b = float, float
-k = (y2 - y1) / (x2 - x1)
-b = y1 - k * x1
-print(f"y={k}*x+{b}")
